fd8a8b42a8f6808967956df34d69eea685824c72
ksenofontovaaa <108142737+ksenofontovaaa@users.noreply.github.com>
2023-01-22 09:31:33 +0200

    cod

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const allGallery = document.querySelector('.gallery');
+
+const creatingOfGallery = galleryItems.map(element=>
+    `<a class="gallery__item" href="${element.original}">
+    <img class="gallery__image" src="${element.preview}" loading = "lazy" alt=${element.description}" />
+  </a>
+    `).join("");
+    allGallery.insertAdjacentHTML('beforeend', creatingOfGallery);
+
+    allGallery.addEventListener("click", onClick);
+
+// function onClick(event) {
+//   event.preventDefault();
+//     if(event.target.nodeName !== 'IMG'){
+//         return
+//     };
+//     const lightbox = new SimpleLightbox('.gallery a', { 
+//         captionsData: 'alt',
+//         captionDelay: 250
+//         });
+// }
